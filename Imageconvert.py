5a406f803e8c6971fb3fe019d702f996871e9375
aliviafisher18-arch <aliviafisher18@gmail.com>
2026-04-29 10:15:32 -0400

    changes

--- Imageconvert.py
+++ Imageconvert.py
@@ -8,6 +8,7 @@ from PIL import Image
 from pathlib import Path
 
 imnum = 1
+<<<<<<< Updated upstream
 for file in Path("cancer_data" + '/' + 'idk what data this is').rglob("*.dcm"):
     category = file.parent.name # determines what category of trials
     print(file)
@@ -24,6 +25,15 @@ for file in Path("cancer_data" + '/' + 'idk what data this is').rglob("*.dcm"):
 
     img = np.squeeze(img)
 
+=======
+for file in Path("cancer_data").rglob("*.dcm"):
+    category = file.parent.name # determines what category of trials
+
+    # 1. Load the DICOM
+    ds = pydicom.dcmread(file)
+    img = ds.pixel_array.astype(float)
+
+>>>>>>> Stashed changes
     # 2. Normalize to 0-255 (Required for PNG)
     # Scaling ensures the highest value is white and lowest is black
     img = (img - np.min(img)) / (np.max(img) - np.min(img)) * 255.0
@@ -33,7 +43,11 @@ for file in Path("cancer_data" + '/' + 'idk what data this is').rglob("*.dcm"):
 
     # 4. Save as PNG
     # If it's a standard scan, use 'L' mode for grayscale
+<<<<<<< Updated upstream
     impath = 'cancer_data' + '/' 'pngs of cancer' + '/' + f"cancer {imnum}.png"
+=======
+    impath = 'cancer_data' + '/'+ f"cancer {imnum}.png"
+>>>>>>> Stashed changes
     final_image = Image.fromarray(img_uint8, mode='L')
     final_image.save(Path(impath))
     imnum +=1
